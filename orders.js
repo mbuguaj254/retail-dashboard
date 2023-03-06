09435519e564c93afdae41295301c647064895b6
mbuguaj254 <85127253+mbuguaj254@users.noreply.github.com>
2023-03-06 11:39:22 +0300

    Dashboard visualizations
    HTML, CSS and JS fully responsive dashboard for a retail setting

---
+++ orders.js
@@ -0,0 +1,39 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber:'83432',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Lavender K343 Drone',
+        productNumber: '45464',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '46432',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '94302',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4K Drone',
+        productNumber: '84736',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Motorola Curve Screen Laptop',
+        productNumber: '23523',
+        paymentStatus: 'Paid',
+        shipping: 'Pending'
+    }
+
+]
